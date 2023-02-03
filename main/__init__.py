f2b0b9de99d87702e5a83b7e0ffe29ed1e414582
Diligent_Coder <84326470+diligent-coder-MANISH@users.noreply.github.com>
2023-02-03 11:35:10 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
- API_HASH = config("API_HASH", default=None)
- BOT_TOKEN = config("BOT_TOKEN", default=None)
- SESSION = config("SESSION", default=None)
- FORCESUB = config("FORCESUB", default=None)
- AUTH = config("AUTH", default=None, cast=int)
+API_ID = 4805297
+API_HASH = "cef82fd6ab4198c068dc0efd8d7b195d"
+BOT_TOKEN = "5518882176:AAEKuV3tpnpTbjSFdKKTybaprvS2Fjdx0N8"
+SESSION = "AQCi_rsBfzo7wh8O5KAa5J8wVr3WrMLjpCBG7hJHeh0KgZdGC0vkzAIWE0jxBr8eR4WvyT06Ea0K6ke-ijfwHmHwqmsSKhX68-XVyxyy6eAZxYpBLI9efRqwco_ttTdqdOlp-NyJM0XLAdXKVMijl9qs3nPO4qmmm0i3kDi0CuCHF968e8QsPS6EJ589tFk690aFIeUXl9zAyyncIKOy0m43GodgSCP6B78bw68e9wsUk9rqztAPpY6EpvK0RJHmWqy5ZRYCOraDOz9-pb8yT7yFvHK9Kc4-RlfOHf4OmkrxBH4XaudhPLqsgKlg-RA3yu04rVTWW2AvaXEJOwdvYIK9WPyD8QA"
+FORCESUB = "savecontbot"
+AUTH = 1492943857
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
